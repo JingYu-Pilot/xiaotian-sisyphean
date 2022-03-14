2383d06945b71c9cf88a6eaf22fa4bc777dfda50
JingYu-Pilot <jyzl.18996134580@gmail.com>
2022-03-14 22:49:01 +0800

    Initial commit

---
+++ src/main/java/com/jingyu/xiaotiansisyphean/XiaotianSisypheanApplication.java
@@ -0,0 +1,16 @@
+package com.jingyu.xiaotiansisyphean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JingYu
+ */
+@SpringBootApplication
+public class XiaotianSisypheanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaotianSisypheanApplication.class, args);
+    }
+
+}
